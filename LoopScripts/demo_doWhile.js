2ea1225acbac8249c3b41b1a802054ee06381d2f
Muhamad Rafiki <rafikimuhammad@gmail.com>
2023-10-30 20:29:41 -0500

    WORKING DO/WHILE

---
+++ LoopScripts/demo_doWhile.js
@@ -0,0 +1,15 @@
+// THE DO/WHILE IS LIKE THE WHILE LOOP EXCEPT 
+//THE CONDITION IS CHECKED AT THE BOTTOM OF THE LOOP 
+
+//SYNTAX 
+// do{
+//     code to be executed
+// }while (condition);
+let num = 1;
+let i =1;
+
+do {
+    num = num * 4;
+    console.log(num);
+    i++;
+}while(i < 5)
