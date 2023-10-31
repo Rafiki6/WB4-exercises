e350dc927bb6c67883578be1c31ec52dc87185ca
Muhamad Rafiki <rafikimuhammad@gmail.com>
2023-10-30 21:41:47 -0500

    for and while  loop

---
+++ LoopScripts/demo_forLoop.js
@@ -0,0 +1,24 @@
+// THE FOR LOOP IS TYPICALLY CONSIDERED A "COUNTING" LOOP
+// IT HAS 3 PARTS SEPARATED BY SEMICOLONS:
+// -CODE THAT EXECUTES BEFORE THE LOOP GEBINS
+// -A CONDITION THAT MUST BE TRUE FOR THE LOOP TO KEEP EXECUTING
+// -CODE THAT RUNS AT THE BOTTOM OF EACH ITERATION
+// SYNTAX
+// for(part 1; part 2;part 3){
+
+// }
+// let num = 1;
+// let i;
+
+// for(i = 0 ; i < 5 ;i ++){
+//     num = num * 2;
+//     console.log(num);
+// }
+// BREAK
+let num1 = 1;
+let a = 1;
+while(a < 100){
+    num1 = num1 * 2; 
+    if(num1 >= 100)break;
+    a++;
+}
