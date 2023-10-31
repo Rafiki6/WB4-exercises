0454d4dde816940753d68c1afd1d4528b4a2454d
Muhamad Rafiki <rafikimuhammad@gmail.com>
2023-10-30 20:19:07 -0500

    WORKING ON WHILE LOOP

---
+++ LoopScripts/demo_whileLoops.js
@@ -0,0 +1,16 @@
+// THERE ARE SEVERAL TYPES OF LOOPS INCLUDE
+// WHILE LOOP, DO/WHILE LOOP,FORLOOP,FOR-INLOOP
+
+// THE WHILE LOOP EXECUTE A BLOCK OF CODE FOR AS LONG AS A SPECIFIED CONDITION IS TRUE
+//SYNTAX
+// while(condition){
+
+// }
+let num = 1;
+let i =1;
+
+while(i < 5){
+    num = num * 2;
+    console.log(num);
+    i++;
+}
