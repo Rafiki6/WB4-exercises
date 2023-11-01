561478427bc63057edf1f25388338eda1a566f37
Muhamad Rafiki <rafikimuhammad@gmail.com>
2023-11-01 12:13:47 -0500

    changed color using js

---
+++ expanding_Arrays/addvalue_Arrays.js
@@ -0,0 +1,7 @@
+// YOU CAN ADD VALUE TO ARRAY AFTER IT HAS BEEN BUILT 
+// BY ASSIGNING A VALUETO AN INDEX NUMBER OUTSIDE THE BOUNDS OF Array 
+let kids = ["Natalie", "Brittany", "Zachary"];
+kids [3]="Natacha";
+kids [4]="Nancy";
+
+console.log(kids)
